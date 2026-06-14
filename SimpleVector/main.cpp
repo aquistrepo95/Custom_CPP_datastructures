39d2ae68a6bcf082b624e66cb7e4edde9502fea7
aquist0295 <aquist@rocketmail.com>
2026-06-14 09:42:52 -0400

    Update to main: uncommenting the final test for copy assignment

--- SimpleVector/main.cpp
+++ SimpleVector/main.cpp
@@ -51,14 +51,14 @@ int main() {
     for(int x = 0; x < vec2.getSize(); x++) {
         std::cout << "Array elements after copy constructor with after pop_back and push_back(60): " << vec2[x] << std::endl;
     }
-/*
+    
     std::cout << "\n" << std::endl;
     std::cout << "################################### copy assignment to vec1 after updates #############################" << std::endl;
     vec1 = vec2;
 
     for(int x = 0; x < vec1.getSize(); x++) {
         std::cout << "Array elements for vec1 obj after copy assignent: " << vec1[x] << std::endl;
-    }*/
+    }
 
     return 0;
 }
